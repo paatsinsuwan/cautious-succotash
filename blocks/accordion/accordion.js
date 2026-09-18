2d4255b1359a011dc352c0013ecb82b63022efdc
paat sinsuwan <paat@sinsuwan.com>
2026-09-18 03:24:46 -0700

    fix: update indentation

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -1,15 +1,15 @@
 function decorateAccordion(el) {
-    const titles = el.querySelectorAll(':scope > div:nth-child(odd)');
-    titles.forEach(title => {
-        title.classList.add('item-title');
-        title.querySelector(':scope > div:last-of-type').remove();
-        // Add a class to the content
-        title.nextElementSibling.classList.add('item-content');
-        // Add a click handler to open the content
-        title.addEventListener('click', () => {
-        title.classList.toggle('open');
-        });
+  const titles = el.querySelectorAll(':scope > div:nth-child(odd)');
+  titles.forEach(title => {
+    title.classList.add('item-title');
+    title.querySelector(':scope > div:last-of-type').remove();
+    // Add a class to the content
+    title.nextElementSibling.classList.add('item-content');
+    // Add a click handler to open the content
+    title.addEventListener('click', () => {
+      title.classList.toggle('open');
     });
+  });
 }
 
 const els = document.querySelectorAll('.accordion');
